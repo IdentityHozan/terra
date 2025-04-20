1be28e2656b3e4fe5c67e0705c3f16ab8fbf00e9
Jay <72636604+IdentityHozan@users.noreply.github.com>
2025-04-19 21:24:05 -0700

    fixes

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,21 +1,21 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    title: 'DRR BLOG',
     author: 'M.F.A',
-    headerTitle: 'Next.js Blog',
+    headerTitle: 'Disaster Risk Management Blogs',
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://nextrixeducation.com', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
+    email: 'support@gmail.com', 
+    github: 'https://github.com/',
+    twitter: 'https://twitter.com/',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
+    youtube: 'https://youtube.com/',
+    linkedin: 'https://www.linkedin.com/',
+    dribbble: 'https://www.nextrixeducation.com',
     locale: 'en-US',
   }
   
