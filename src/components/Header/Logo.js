d223423a67edc35eabf84f64fbdbbfe72e005a2a
Jay <72636604+IdentityHozan@users.noreply.github.com>
2025-05-06 05:52:41 -0700

    Update Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import profileImg from "@/public/profile-img.png"
 
-const Logo = () => {!
+const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
         <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
